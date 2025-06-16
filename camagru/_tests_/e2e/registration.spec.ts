a7cc89ddc6f1b9182d59cfa207eda9be80d25047
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-06-16 14:30:29 +0300

    e2e tests attempts

--- camagru/_tests_/e2e/registration.spec.ts
+++ camagru/_tests_/e2e/registration.spec.ts
@@ -15,3 +15,31 @@ test('all registration flow', async ({ page }) => {
   await page.waitForURL('**/auth/notification')
   await expect(page.getByText('Thank you for registering!')).toBeVisible()
 })
+
+// test('all signin flow', async ({ page, request }) => {
+//   await request.post('http://localhost:3001/api/auth/register', {
+//     data: {
+//       email: 'user@example.com',
+//       username: 'username',
+//       password: 'ValidPass123!',
+//     },
+//     headers: { 'Content-Type': 'application/json' },
+//   })
+
+//   await request.post('http://localhost:3001/api/test/verify-email', {
+//     data: { email: 'user@example.com' },
+//     headers: { 'Content-Type': 'application/json' },
+//   })
+
+//   await page.goto('/auth/signin')
+
+//   await page.getByPlaceholder('Username').fill('username')
+//   await page.getByPlaceholder('Password').fill('ValidPass123!')
+
+//   const submitButton = await page.getByTestId('signin-button')
+//   await expect(submitButton).not.toBeDisabled()
+//   await submitButton.click()
+
+//   await page.waitForURL('**/user/')
+//   await expect(page.getByText('username')).toBeVisible()
+// })
