80e91f7f64a4df5e44b91153880b1169af9bd600
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-06-29 16:10:44 +0300

    fix in styling comment form when comment is long

--- camagru/components/posts/AddCommentForm.tsx
+++ camagru/components/posts/AddCommentForm.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import styles from '@/styles/CommentForm.module.css'
 import { FaArrowUp } from 'react-icons/fa'
 import { type Comment } from '@/types/comment'
 
 const MAX_COMMENT_LENGTH = 2200
+const MAX_ROWS = 3
 
 type CommentFormProps = {
   postId: string
@@ -19,6 +20,30 @@ export default function CommentForm({
   const [newComment, setNewComment] = useState('')
   const [isSubmitting, setIsSubmitting] = useState(false)
   const [error, setError] = useState('')
+
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null)
+
+  const autoResize = () => {
+    const textarea = textareaRef.current
+    if (textarea) {
+      textarea.style.height = 'auto'
+
+      const lineHeight = parseInt(
+        getComputedStyle(textarea).lineHeight || '20',
+        10
+      )
+      const maxHeight = lineHeight * MAX_ROWS
+
+      textarea.style.overflowY = 'hidden'
+
+      if (textarea.scrollHeight > maxHeight) {
+        textarea.style.height = `${maxHeight}px`
+        textarea.style.overflowY = 'auto'
+      } else {
+        textarea.style.height = `${textarea.scrollHeight}px`
+      }
+    }
+  }
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
@@ -58,11 +83,13 @@ export default function CommentForm({
     <form onSubmit={handleSubmit} className={styles.commentForm}>
       <div className={styles.inputWrapper}>
         <textarea
+          ref={textareaRef}
           value={newComment}
           maxLength={MAX_COMMENT_LENGTH + 100}
           onChange={(e) => {
             setNewComment(e.target.value)
             if (error) setError('')
+            autoResize()
           }}
           placeholder="Write a comment..."
           rows={1}
