cb9ef578d842ce055da1430e2844ab8bafcee4e9
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-08-20 14:10:04 +0300

    fixing

--- camagruApp/pages/api/auth/register.ts
+++ camagruApp/pages/api/auth/register.ts
@@ -90,7 +90,7 @@ export default async function handler(
 
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_SERVER_HOST,
-      port: Number(process.env.EMAIL_EMAIL_SERVER_PORT),
+      port: Number(process.env.EMAIL_SERVER_PORT),
       secure: Number(process.env.EMAIL_SERVER_PORT) === 465,
       auth: {
         user: process.env.EMAIL_SERVER_USER,
