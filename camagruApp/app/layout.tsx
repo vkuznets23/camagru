7fb55b7bf80e92285481327c878e407399e1f939
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-08-20 10:19:24 +0300

    added meta

--- camagruApp/app/layout.tsx
+++ camagruApp/app/layout.tsx
@@ -20,6 +20,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="color-scheme" content="light dark" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
       </head>
       <body>
         <Providers>
